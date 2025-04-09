303bb3e97d6de88c510b386cef2a61be8817f995
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-09 00:00:57 -0400

    Sync and COMPLETED

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays; 
+//cant sync work
+
 public class PracticeProblem {
+    private static String sortedInputString;
+    private static ArrayList<String> generatedPermutations;
 
-	public static void main(String args[]) {
+    public static void permutate(String currentPermutation, int visitedMask, boolean isUnique) {
+        if (currentPermutation.length() == sortedInputString.length()) {
+            generatedPermutations.add(currentPermutation);
+            return;
+        }
+        
+        boolean[] usedCharacters = new boolean[256];
+        for (int i = 0; i < sortedInputString.length(); ++i) {
+            int currentChar = sortedInputString.charAt(i);
+            if ((visitedMask & (1 << i)) == 0) {
+                if ((isUnique && !usedCharacters[currentChar]) || !isUnique) {
+                    permutate(currentPermutation + sortedInputString.charAt(i), visitedMask | (1 << i), isUnique);
+                } else {
+                    continue;
+                }
+                if (isUnique) {
+                    usedCharacters[currentChar] = true;
+                }
+            }
+        }
+    }
 
-	}
+    public static ArrayList<String> perms(String inputString) {
+        char[] charArray = inputString.toCharArray();
+        Arrays.sort(charArray);
+        sortedInputString = new String(charArray);
+        generatedPermutations = new ArrayList<String>();
+        permutate("", 0, false);
+        return generatedPermutations;
+    }
 
-	
-
+    public static ArrayList<String> permsUnique(String inputString) {
+        char[] charArray = inputString.toCharArray();
+        Arrays.sort(charArray);
+        sortedInputString = new String(charArray);
+        generatedPermutations = new ArrayList<String>();
+        permutate("", 0, true);
+        return generatedPermutations;
+    }
 }
